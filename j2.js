20059d9471603d7fed099b0cc61937bf0bb3e51b
mr-morfeus <wwwmorfeus@yandex.ru>
2025-11-28 15:54:33 +0400

    111

--- j2.js
+++ j2.js
@@ -6,17 +6,51 @@ const employees = [
   { name: 'Петр', department: 'Finance' },
   { name: 'Ольга', department: 'HR' }
 ];
-
+//console.log("sdfs377733f", employees[0].department);
+/*
 function groupByDepartment(employees){
+const group = {};
 
- let mas = employees.reduce((acc, nam) => {
-    if(!acc[nam.department]){
-        acc[nam.department] = [];
+  for(let i = 0; i < employees.length; i++){
+    
+    if(!group[employees[i].department]){
+      console.log("Cоздаю массив", employees[i].department);
+      group[employees[i].department] = [];
     }
-  acc[nam.department].push(nam.name);
-return acc;
-  },{})
-  return mas
+    console.log("Помещаю в массив", employees[i].name);
+    group[employees[i].department].push(employees[i].name);
+  };
+  
+  return group
 }
-let res = groupByDepartment(employees);
-console.log(res)
+const res = groupByDepartment(employees);
+console.log("Результат", res);*/
+/*
+function grobs(employees){
+  const group = {};
+  for(let element of employees){
+    console.log(element.department); 
+    if(!group[element.department]){
+       group[element.department] = [];    
+    }
+    group[element.department].push(element.name);
+  }
+return group
+}
+
+console.log("Результат", grobs(employees));*/
+function grobs(employees){
+  const group = {};
+  let count= 0;
+  while (count < employees.length) {
+    console.log("count", count);
+    if(!group[employees[count].department]){
+        group[employees[count].department] = [];
+        count++;
+    }
+    group[employees[count].department].push(employees[count].name);
+    count++;
+  }
+   return group
+}
+console.log("Результат", grobs(employees));
